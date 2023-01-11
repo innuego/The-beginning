c252491273003bc0180d9257f2837765aaf03adf
innuego <davyzadolbaly@gmail.com>
2023-01-11 15:36:41 +0300

    YANEPONYMAYU

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,13 +1,16 @@
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
     @Test
-    public void testHelloWorld(){
-        Response response = RestAssured
-                .get("https://playground.learnqa.ru/api/get_text")
-                .andReturn();
-        response.prettyPrint();
+    public static void main (String[] args){
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+        Object testObject = response.get("messages.find {msg -> msg.message == 'And this is a second message'}");
+
+        System.out.println(testObject);
     }
+
 }
