4502d50f970c1ff5c7ec017ad65fef843979d0da
innuego <davyzadolbaly@gmail.com>
2023-01-11 18:59:31 +0300

    Poshady

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,16 +1,41 @@
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
     @Test
-    public static void main (String[] args){
-        JsonPath response = RestAssured
-                .get("https://playground.learnqa.ru/api/get_json_homework")
-                .jsonPath();
-        Object testObject = response.get("messages.find {msg -> msg.message == 'And this is a second message'}");
+    public void main (){
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+        response.prettyPrint();
 
-        System.out.println(testObject);
+        String locationHeader = response.getHeader("Location");
+        int statusCode = response.getStatusCode();
+
+        while (statusCode != 200) {
+            Response response200 = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .when()
+                    .get(locationHeader)
+                    .andReturn();
+            locationHeader = response200.getHeader("Location");
+            statusCode = response200.getStatusCode();
+            String locationStack = response200.header("Name");
+            if (locationHeader == null) {
+                System.out.println("Боже, храни CRM. Мы прибыли!");
+                System.out.println("Status code "+statusCode);
+                break;
+            } else {
+                System.out.println("Перенаправление на "+locationHeader);}
+            System.out.println("Status code "+statusCode);
+        }
     }
-
 }
